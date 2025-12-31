a18ecd70d30a923395fb18ea08164ee8b13fa930
meraladvertisement-meral <meral.advertisement@gmail.com>
2025-12-31 11:27:45 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // نستخدم 'process.env': 'process.env' أو كائن فارغ كحماية إضافية
-    'process.env': {
-      API_KEY: JSON.stringify(process.env.API_KEY)
-    }
-  }
 });
